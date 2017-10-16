69c831c01c026282394aa78f8765d0ceec12b48c
lacherv <awelakoue@gmail.com>
2017-10-16 22:30:26 +0200

    modified...

--- 02.Data Structures/Tuples/TupleDemo.py
+++ 02.Data Structures/Tuples/TupleDemo.py
@@ -1,1 +1,35 @@
+tuple1 = ("green", "red", "blue")  # Create a tuple
+print(tuple1)
+
+tuple2 = tuple([7, 1, 2, 23, 4, 25])  # Create a tuple from a list
+print(tuple2)
+
+print("length is", len(tuple2))  # Use function len
+print("max is", max(tuple2))  # Use max
+print("min is", min(tuple2))  # Use min
+print("Sum is", sum(tuple2))  # Use sum
+
+print("The first element is ", tuple2[0])  # Use index operator
+
+tuple3 = tuple2 + tuple1  # Combine two tuples
+print(tuple3)
+
+tuple3 = 2 * tuple1  # Duplicate a tuple
+print(tuple1[-1])
+
+print(tuple2[2: 4])  # Slicing operator
+print(tuple1[-1])
+
+print(2 in tuple2)  # in operator
+
+for v in tuple1:
+    print(v, end=' ')
+print()
+
+list1 = list(tuple2)
+list1.sort()
+tuple4 = tuple(list1)
+tuple5 = tuple(list1)
+print(tuple4)
+print(tuple4 == tuple5)
 
