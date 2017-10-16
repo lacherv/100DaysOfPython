ebd7b1771317fc0dcc6b84ae635cd6078352401d
lacherv <awelakoue@gmail.com>
2017-10-16 21:50:31 +0200

    modified ..

--- 02.Data Structures/Lists/shopping_list.py
+++ 02.Data Structures/Lists/shopping_list.py
@@ -1,1 +1,20 @@
+# make a list to hold onto our items
+shopping_list = []
+# print out the instructions on how to use the app
+print("What should we ick up at the store?")
+print("Enter 'DONE' to stop adding items.")
 
+while True:
+    # ask for new items
+    new_item = input("> ")
+
+    # add new items to our list
+    shopping_list.append(new_item)
+
+# be able to quit the app
+
+# print out the list
+print("Here's your list:")
+
+for item in shopping_list:
+    print(item)
