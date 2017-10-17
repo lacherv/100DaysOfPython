1279d500d0bae500f6aee90a47a9ff4cb7bb8809
lacherv <awelakoue@gmail.com>
2017-10-17 11:21:23 +0200

    Create a conditianals directory

---
+++ 01.Loops/fibonacci.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# simple fibonacci series
+# the sum of two elements defines the next set
+
+a, b = 0, 1
+while b < 50:
+    print(b , end=' ')
+    a, b =  b, a + b
+print("\nDone.")
